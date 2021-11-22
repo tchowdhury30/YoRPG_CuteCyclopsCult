d425351606dd0bdbdf126946c8078a395bddd9ce
tchowdhury30 <90805264+tchowdhury30@users.noreply.github.com>
2021-11-22 14:01:24 -0500

    hehe

---
+++ v1/Monster.java
@@ -0,0 +1,8 @@
+public class Monster{
+
+	private int health;
+
+	public boolean isAlive(){
+                return (health > 0);
+        }
+}
---
+++ v1/Protagonist.java
@@ -0,0 +1,29 @@
+public class Protagonist{
+
+	private int health;
+	private int atkDmg;
+
+	public Protagonist( String name ){
+		health = 0;
+	}
+
+	// Give user the option of using a special attack:
+        // If you land a hit, you incur greater damage,
+        // ...but if you get hit, you take more damage
+
+	public boolean isAlive(){
+		return (health > 0);
+	}
+
+	public void specialize(){
+	// Give user the option of using a special attack:
+        // If you land a hit, you incur greater damage,
+        // ...but if you get hit, you take more damage
+
+	
+	}
+
+	public void normalize(){
+		
+	}
+}
