8fb4cbed5fca240eee5c6d1599df7cd8c81aadb1
Tasnim <tchowdhury30>
2021-11-24 14:00:20 -0500

    l

---
+++ v3/Barbarian.java
@@ -0,0 +1,19 @@
+public class Barbarian extends Protagonist {
+    public static String about(){
+        String description = "Barbarians cometh yonder and carry with them blessings of high defense.";
+        return description;
+    }
+    
+    public Barbarian(){
+        health = 100;
+        strength = 40;
+        defense = 40;
+        atkRate = 0.6;
+    }
+
+    public Barbarian( String nameInp) {
+        this(); 
+        name = nameInp;
+    }
+}
+
---
+++ v3/Paladin.java
@@ -0,0 +1,20 @@
+public class Paladin extends Protagonist {
+
+    public Paladin() {
+        health = 100;
+        strength = 50;
+        defense = 16;
+        atkRate = 0.6;
+    }
+
+    public Paladin ( String nameInp ) {
+        this(); 
+        name = nameInp;
+    }
+
+    public static String about(){
+        String description = "Paladins art knights of Ye Grand Order. They dost deal high damage to monsters.";
+        return description;
+    }
+    
+}
